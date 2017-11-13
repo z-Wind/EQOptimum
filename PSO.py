c046daec2c6218277a3fec9fa0c87bea0b30ff2f
子風 <zpsyhapcst@gmail.com>
2017-11-13 19:14:42 +0800

    加入定期隨機

--- PSO.py
+++ PSO.py
@@ -6,6 +6,7 @@ class PSO:
     gBest = None
     gBestFitness = np.inf
     
+    
     # 初始化
     def __init__(self, fitness, bounds, swarmSize=100, w=0.5, wp=0.5, wg=0.5):
         '''
@@ -20,18 +21,65 @@ class PSO:
         self.wg = wg
         
         # 初始化粒子和速度
-        self.particles = np.zeros((self.swarmSize, self.pNum))
-        
-        self.v = np.zeros((self.swarmSize, self.pNum))
-        for i, b in enumerate(self.bounds):
-            self.particles[:,i] = np.random.uniform(b[0], b[1], self.swarmSize)
-            self.v[:,i] = np.random.uniform(-b[1]+b[0], b[1]-b[0], self.swarmSize)
+        self.init_particles()
 
         # 初始化 fitness
         self.pBest = np.zeros((self.swarmSize, self.pNum))
         self.pBestFitness = np.ones(self.swarmSize) * np.inf
         self.updateFitness()
+    
+    
+    # 初始化粒子
+    def init_particles(self):
+        self.particles = np.zeros((self.swarmSize, self.pNum))        
+        self.v = np.zeros((self.swarmSize, self.pNum))
         
+        for i, b in enumerate(self.bounds):
+            self.particles[:,i] = np.random.uniform(b[0], b[1], self.swarmSize)
+            self.v[:,i] = np.random.uniform(-b[1]+b[0], b[1]-b[0], self.swarmSize)
+            
+            
+    # 增加粒子
+    def produce_particles(self, addSize):
+        self.swarmSize += addSize
+        ps = np.zeros((addSize, self.pNum))        
+        vs = np.zeros((addSize, self.pNum))
+        pbests = np.zeros((addSize, self.pNum))
+        pBestFitness = np.ones(addSize) * np.inf
+        
+        for i, b in enumerate(self.bounds):
+            ps[:,i] = np.random.uniform(b[0], b[1], addSize)
+            vs[:,i] = np.random.uniform(-b[1]+b[0], b[1]-b[0], addSize)
+            
+        self.particles = np.concatenate((ps, self.particles))
+        self.v = np.concatenate((vs, self.v))
+        self.pBest = np.concatenate((pbests, self.pBest))
+        self.pBestFitness = np.concatenate((pBestFitness, self.pBestFitness))
+        
+        
+    # 取代粒子
+    def replace_particles(self, replaceSize):
+        # 重新排序
+        index = np.argsort(self.pBestFitness)[::-1]
+        self.particles = self.particles[index]
+        self.v = self.v[index]
+        self.pBest = self.pBest[index]
+        self.pBestFitness = self.pBestFitness[index]
+        
+        ps = np.zeros((replaceSize, self.pNum))        
+        vs = np.zeros((replaceSize, self.pNum))
+        pbests = np.zeros((replaceSize, self.pNum))
+        pBestFitness = np.ones(replaceSize) * np.inf
+        
+        for i, b in enumerate(self.bounds):
+            ps[:,i] = np.random.uniform(b[0], b[1], replaceSize)
+            vs[:,i] = np.random.uniform(-b[1]+b[0], b[1]-b[0], replaceSize)
+
+        self.particles[:replaceSize] = ps
+        self.v[:replaceSize] = vs
+        self.pBest[:replaceSize] = pbests
+        self.pBestFitness[:replaceSize] = pBestFitness
+    
     
     # 更新 fitness
     def updateFitness(self):
@@ -46,30 +94,43 @@ class PSO:
                     self.gBestFitness = fit
         
         
-    def run(self, threshold=0.01, updateThreshold=1e-4, maxiter=20):
+    def run(self, threshold=0.01, maxiter=100, randIter=20):
         n = 0
-        while self.gBestFitness > threshold and n < maxiter:
-            # 更新粒子速度
-            rp = np.random.rand()
-            rg = np.random.rand()
-            self.v = self.w*self.v + self.wp*rp*(self.pBest-self.particles) + self.wg*rg*(self.gBest-self.particles)
-            
-            # 更新粒子
-            self.particles = self.particles + self.v
-            for i, b in enumerate(self.bounds):
-                self.particles[:,i] = np.clip(self.particles[:,i], b[0], b[1])
-            
-            # 計算 fitness
-            old = self.gBestFitness
-            self.updateFitness()
-            print(self.gBestFitness)
-            
-            if old - self.gBestFitness < 0.001:
-                n += 1
-            else:
-                n = 0
+        try:
+            while self.gBestFitness > threshold and n < maxiter:
+                # 更新粒子速度
+                rp = np.random.rand()
+                rg = np.random.rand()
+                self.v = self.w*self.v + self.wp*rp*(self.pBest-self.particles) + self.wg*rg*(self.gBest-self.particles)
+                
+                # 更新粒子
+                self.particles = self.particles + self.v
+                for i, b in enumerate(self.bounds):
+                    self.particles[:,i] = np.clip(self.particles[:,i], b[0], b[1])
+                
+                # 計算 fitness
+                old = self.gBestFitness
+                self.updateFitness()
+                print(self.gBestFitness)
+                
+                if old - self.gBestFitness < threshold/10:
+                    n += 1
+                    if n % randIter == 0:
+                        self.replace_particles(self.swarmSize//3*2)
+                        print('replace, now Size: {}'.format(self.swarmSize))
+                        self.produce_particles(self.swarmSize//10)
+                        print('produce, now Size: {}'.format(self.swarmSize))                        
+                else:
+                    n = 0
+                if n > maxiter:
+                    break                
+                print(n)
+                
+        except KeyboardInterrupt:
+            print("Interrupt by user")
             
         return self.gBest, self.gBestFitness
+        
         
 if __name__ == "__main__":
     def fitness(x):
--- psoDemo.py
+++ psoDemo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import freqz
-from pyswarm import pso
+from collections import Counter
 
 import filters
 import PSO
@@ -15,7 +15,7 @@ def filterModel(x):
 
     for fc, BW, gain in x:
         b, a = filters.bandpass_peaking(fc=fc, gain=gain, BW=BW)
-        w, h = freqz(b, a, worN=np.linspace(np.pi*2/fs*20, np.pi*2/fs*20e3, 1000))
+        w, h = freqz(b, a, worN=np.pi*2/fs*np.logspace(np.log10(20), np.log10(20e3), 1000))
         db = 20 * np.log10(abs(h))
         
         w_final = w
@@ -26,42 +26,46 @@ def filterModel(x):
     return w_final*fs/(2*np.pi), db_final
     
 # target
-# paras = [(1e4, 2500, 3), (300, 201, 10), (400, 600, 5), (600, 200, 8), 
-         # (2000, 3500, 13), (6000, 4000, 3), (8500, 6000, 2.75),]
-paras = [(1e4, 2500, 3), (300, 201, 10)] 
+paras = [(1e4, 2500, 3), (300, 201, 10), (400, 600, 5), (600, 200, 8), 
+         (2000, 3500, 13), (6000, 4000, 3), (8500, 6000, 2.75),]
+# paras = [(1e4, 2500, 3), (300, 201, 10), (400, 600, 5), (600, 200, 8),] 
+paras = np.array(paras)
 f_t, db_t = filterModel(paras)   
 
 def fitness(x):   
     # compare
     x = np.array(x).reshape(-1, 3)
-    x[:,0] = x[:,0]**10
-
+    
+    x[:,0] = paras[:,0]
     f_c, db_c = filterModel(x)
     global f_t, db_t
     # 計算 mse
     mse = ((db_t - db_c) ** 2).mean()
     
-    return  mse
+    # 計算符合程度
+    familiar = 1/(np.sum(db_c-db_t < 0.01)+1)*100
+    
+    # 計算頻率分散
+    f_count = Counter(np.round(x[:,0]))
+    sameCount = len([item for item, count in f_count.items() if count > 1])
+    
+    # print("fit {} {} {}".format(mse, familiar, sameCount))
+    fit = mse + familiar + sameCount
+    return fit
 
-filterNum = len(paras)
-xopt, fopt = pso(fitness, (np.log10(20),100,1)*filterNum, (np.log10(20e3),10000,20)*filterNum)
-print('pyswarm', fopt)
-opt = np.array(xopt).reshape(-1, 3)
-opt[:,0] = opt[:,0]**10
-print(opt)
-f, db = filterModel(opt)
-plt.semilogx(f, db, label="pyswarm")
 
-pso = PSO.PSO(fitness, [(np.log10(20),np.log10(20e3)), (100,10000), (1,20)]*filterNum)
+
+pso = PSO.PSO(fitness, [(20,10e3), (100,10000), (1,20)]*len(paras), swarmSize=100, w=0.5, wp=0.5, wg=0.5)
 opt, optfit = pso.run(threshold=1e-2)
 print(optfit)
 opt = np.array(opt).reshape(-1, 3)
-opt[:,0] = opt[:,0]**10
-print(opt)
+opt = opt.astype(int)
+opt[:,0] = paras[:,0]
+print(opt[np.argsort(opt[:, 0])])
 f, db = filterModel(opt)
 plt.semilogx(f, db, label="PSO")
 
-plt.semilogx(f_t, db_t, label="target", color='red')
-
+plt.semilogx(f_t, db_t, linestyle='--', label="target")
 plt.legend()
 plt.show()
+#1.31
